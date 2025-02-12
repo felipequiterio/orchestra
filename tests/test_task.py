c0d3993525c53d2554a83e058e29504adaeb1b25
Felipe Quiterio <felipe.quiterio@ip.tv>
2025-02-12 00:32:15 -0500

    Creating tests in route function

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,5 +1,6 @@
-from core.task import generate, TaskList
-from tests.mocks import agent_list
+from core.task import generate, TaskList, route
+from tests.mocks import agent_list, task_list
+import pytest
 
 def test_task_generation():
     query = "Add 'Buy groceries' to the user's to-do list and also fetch the current weather for New York City"
@@ -8,3 +9,14 @@ def test_task_generation():
     assert len(task_list.steps) == 2
     assert task_list.steps[0].task == "Add 'Buy groceries' to the user's to-do list"
     assert task_list.steps[1].task == "Fetch the current weather for New York City"
+
+@pytest.mark.focus
+def test_route():
+    results = route(task_list, agent_list)
+    
+    assert len(results) == 2
+    
+    for result in results:
+        assert result['status'] == 'success'
+        assert isinstance(result['result'], dict)
+        assert 'is_async' in result
