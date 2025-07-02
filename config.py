216c8915f27ed31cf3a778e2fbc42da97eabce5c
Felipe <felipe.quiterio@hotmail.com>
2025-07-02 13:46:58 -0400

    Implemented QWEN model

--- config.py
+++ config.py
@@ -6,4 +6,6 @@ load_dotenv()
 OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
 OPENAI_MODEL = os.getenv("OPENAI_MODEL")
 DEEPSEEK_MODEL = os.getenv("DEEPSEEK_MODEL")
+QWEN_MODEL = os.getenv("QWEN_MODEL")
+
 TEMPERATURE = os.getenv("TEMPERATURE")
